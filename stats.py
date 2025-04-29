c1309073807cc9c30858cf3400384c728c2b8fce
JimenezAlejandro <17188622+JimenezAlejandro@users.noreply.github.com>
2025-04-29 10:06:35 -0700

    add get_character_count function

--- stats.py
+++ stats.py
@@ -1,2 +1,11 @@
 def get_num_words(text):
     return len(text.split())
+
+def get_character_count(text):
+    character_counts = {}
+    for char in text:
+        char_lower = char.lower()
+        if char_lower not in character_counts:
+            character_counts[char_lower] = 0
+        character_counts[char_lower] += 1
+    return character_counts

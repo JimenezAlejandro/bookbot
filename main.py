11b2f6d187217f22d99c832355879b0a3cd6db63
JimenezAlejandro <17188622+JimenezAlejandro@users.noreply.github.com>
2025-04-29 10:08:19 -0700

    add get_sorted_char_count function, rename get_character_count to get_char_count_dict, and print report

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from stats import get_num_words, get_character_count
+from stats import get_num_words, get_char_count_dict, get_sorted_char_count
 
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    char_count = get_character_count(text)
-    print(f"{num_words} words found in the document")
-    print(f"{char_count}")
+    char_count = get_char_count_dict(text)
+    sorted_chars = get_sorted_char_count(char_count)
+    print_divider_with_text("BOOKBOT", "=", 12)
+    print(f"Analyzing book found at {book_path}...")
+    print_divider_with_text("Word Count", "-", 11)
+    print(f"Found {num_words} total words")
+    print_divider_with_text("Character Count", "-", 9)
+    for d in sorted_chars:
+        if d["char"].isalpha():
+            print(f"{d["char"]}: {d["num"]}")
+    print_divider_with_text("END", "=", 13)
+    
+def print_divider_with_text(text, divider, divider_count):
+    print(f"{divider * divider_count} {text} {divider * divider_count}")
 
 def get_book_text(filepath):
     with open(filepath) as f:
--- stats.py
+++ stats.py
@@ -1,7 +1,7 @@
 def get_num_words(text):
     return len(text.split())
 
-def get_character_count(text):
+def get_char_count_dict(text):
     character_counts = {}
     for char in text:
         char_lower = char.lower()
@@ -9,3 +9,13 @@ def get_character_count(text):
             character_counts[char_lower] = 0
         character_counts[char_lower] += 1
     return character_counts
+
+def sort_on(dict):
+    return dict["num"]
+
+def get_sorted_char_count(char_dict):
+    sorted = []
+    for char in char_dict:
+        sorted.append({ "char": char, "num": char_dict[char] })
+    sorted.sort(reverse=True, key=sort_on)
+    return sorted

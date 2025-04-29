cd8aa484ca414dd48a38bf9be89b5b8c214ce7bd
JimenezAlejandro <17188622+JimenezAlejandro@users.noreply.github.com>
2025-04-29 10:21:38 -0700

    add cli argument parsing

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import sys
 from stats import get_num_words, get_char_count_dict, get_sorted_char_count
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     char_count = get_char_count_dict(text)
